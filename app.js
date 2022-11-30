bb28fd105fcec0bf9ff2bea9fc12bbf248c66c57
Karl Mattias Pärloja <75037556+KarlStallion@users.noreply.github.com>
2022-11-30 14:08:02 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,86 @@
+function minJatund(p1){
+    const d = new Date(p1*1000);
+    var hour1 = d.getHours();
+    var minute1 = d.getMinutes();
+
+    return hour1+':'+minute1;
+}
+
+var dict = { 
+  "Clouds" : 'logod/few_clouds.png' , 
+  "Clear" : 'logod/clear.png' ,
+  "Atmosphere" : 'logod/mist.png' ,
+  "Snow" : 'logod/snow.png' ,
+  "Rain" : 'logod/shower.png',
+  "Drizzle" : 'logod/rain.png',
+  "Thunderstorm" : 'logod/thunderstorm.png',
+}; 
+
+var nupp = document.querySelector('.kodu-nupp')
+var sisendValue = document.querySelector('.sisendValue')
+
+var linn = document.querySelector('.linn')
+var temp = document.querySelector('.temperatuur')
+var tuul = document.querySelector('.tuul')
+var rohk = document.querySelector('.rohk')
+var tunne = document.querySelector('.tunne')
+
+var tous = document.querySelector('.tous')
+var loojang = document.querySelector('.loojang')
+
+var maks = document.querySelector('.kodu-text11')
+var mini = document.querySelector('.kodu-text14')
+
+nupp.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+sisendValue.value+'&units=metric&appid=29716e7c7eb08f50d29ff5b0b9df994d')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            var linnValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var tuulValue = data['wind']['speed'];
+            var rohkValue = data['main']['pressure'];
+            var tunneValue = data['main']['feels_like'];
+
+            var toushValue = data['sys']['sunrise'];
+            var setValue = data['sys']['sunset'];
+
+            var ikoonValue = data['weather']['0']['main'];
+
+            var maksValue = data['main']['temp_max'];
+            var minValue = data['main']['temp_min'];
+            
+
+            linn.innerHTML = linnValue;
+            temp.innerHTML = Math.round(tempValue)+'°';
+            tuul.innerHTML = tuulValue+' m/s';
+            rohk.innerHTML = rohkValue+' hPa';
+            tunne.innerHTML = tunneValue+'°';
+
+            tous.innerHTML = minJatund(toushValue);
+            loojang.innerHTML = minJatund(setValue);
+            
+            maks.innerHTML = 'Max: '+maksValue+'°';
+            mini.innerHTML = 'Min: '+minValue+'°';
+
+            var ilmValue = dict[ikoonValue];
+            console.log(ilmValue);
+            document.getElementById("peaikoon").src=ilmValue;
+
+
+        })
+.catch(err => alert('Vale linn'))
+})
+
+var input = document.getElementById("linn_sisend");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("nupp").click();
+  }
+});
